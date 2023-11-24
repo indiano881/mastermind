82a1ef16f9af550aff2da571e37864209556784a
Davide Baldi <d.baldi88@gmail.com>
2023-11-24 13:49:00 +0100

    pc array generator done

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-console.log("connected")
+
 //MASTERMIND
 
 const colourDatabase = ["red","yellow", "green", "blue", "pink","white"];
@@ -6,3 +6,8 @@ const colourDatabase = ["red","yellow", "green", "blue", "pink","white"];
 
 
 //generated a random array with 4 colour
+let computerColourChoice = ["empty-colour","empty-colour","empty-colour","empty-colour",];
+for (let i=0; i<computerColourChoice.length; i++) {
+    computerColourChoice[i]=colourDatabase[Math.floor((Math.random()*colourDatabase.length))];
+}
+console.log(computerColourChoice)
