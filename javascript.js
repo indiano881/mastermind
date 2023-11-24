11eb8e91efc01ad8c7177034945b6cacf2e2ab59
Davide Baldi <d.baldi88@gmail.com>
2023-11-24 13:54:10 +0100

    user empty array created- start while loop logic

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 
 //MASTERMIND
 
-const colourDatabase = ["red","yellow", "green", "blue", "pink","white"];
+const COLOUR_DATABASE = ["red","yellow", "green", "blue", "pink","white"];
 
 
 
 //generated a random array with 4 colour
-let computerColourChoice = ["empty-colour","empty-colour","empty-colour","empty-colour",];
+let computerColourChoice = ["empty-colour","empty-colour","empty-colour","empty-colour"];
+
 for (let i=0; i<computerColourChoice.length; i++) {
-    computerColourChoice[i]=colourDatabase[Math.floor((Math.random()*colourDatabase.length))];
+
+    computerColourChoice[i]=COLOUR_DATABASE[Math.floor((Math.random()*COLOUR_DATABASE.length))];
 }
-console.log(computerColourChoice)
+
+console.log(computerColourChoice);
+//user choice
+alert("***** Welcome at Mastermind *****")
+
+let userColourChoice = ["empty-colour","empty-colour","empty-colour","empty-colour"];
