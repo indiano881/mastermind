937787e3f7a67e1aeb5c4143325116ce3323850b
Davide Baldi <d.baldi88@gmail.com>
2023-11-30 10:22:16 +0100

    code has been cleaned up

--- javascript.js
+++ javascript.js
@@ -17,15 +17,15 @@ let roundCounter = 1;
 //functions
 const displayAlertWrongInput = (alertString = "") => alert(`Sorry Input NOT VALID ${alertString} `);
 
-//user: welcome and choiche of coulors
+//user: welcome and choiches of coulors
 alert("***** Welcome at Mastermind *****");
-
 alert("***Instructions: guess the 4 colours the computer has choosen. ***" +
 "\n ***Choose within: " + COLOUR_DATABASE.join(" ") +
 "***\n***Hard level adds: " + EXTRA_COLOUR_ONE +
 "Level mastermind adds: " + EXTRA_COLOUR_ONE + " and " + EXTRA_COLOUR_TWO +
  "*** \n***The colours choosen by the computer will be ALL different***");
 
+//Difficulty-level choiches with validation
 do {
     difficultyLevelChoiche=prompt("Choose a level within: \n easy (max 14 attempts) \n medium (max 10 attempts) \n hard (max 7 attempts + 1 extra colour) \n mastermind (max 4 attempts + 2 extra colours)");
 
@@ -37,6 +37,7 @@ do {
 
 } while (!difficultyLevelChoiche || LEVELS_DATABASE.includes(difficultyLevelChoiche)===false) 
 
+//Difficulty-level menu
 switch (difficultyLevelChoiche) {
     case "easy":
         runningGameLevelChoiche = 14;
@@ -59,19 +60,21 @@ switch (difficultyLevelChoiche) {
     default:
         break;
 }
+
 //Computer colour array generator
 for (let i = 0; i < computerColourChoice.length; i++) {
     removeColourIndex = Math.floor(Math.random() * copy_COLOUR_DATABASE.length);
     computerColourChoice[i] = copy_COLOUR_DATABASE.splice(removeColourIndex, 1)[0];
 }
 
-
+//Mastermind game
 do {
 
     for ( let i=0; i<userColourChoice.length; i++ ) {
-
+        
+        //User´choiches with validation
         do {
-            
+           
             userColourChoice[i] = prompt("Choose within: \n" + COLOUR_DATABASE.join(" ") + 
             "\n at position:\n " + (i+1) +
             "/4 \n Your choiches are ==>\n " + userColourChoice.join("--") +
@@ -79,39 +82,45 @@ do {
             "----------");
 
             if (!userColourChoice[i]) {
+                
                     displayAlertWrongInput("To quit the game exit or refresh page!");
+
                 } else if (COLOUR_DATABASE.includes(userColourChoice[i])===false) {
+
                     displayAlertWrongInput("Valid inputs are: \n" + COLOUR_DATABASE.join(" ") + "\nInputs are case sensitive");
+
                 }
 
         } while (!userColourChoice[i] || COLOUR_DATABASE.includes(userColourChoice[i])===false)
 
+        //User´s input-computer colours match
         if (userColourChoice[i]===computerColourChoice[i]) {
 
-                console.log(userColourChoice[i] + " is CORRECT in place n." + (i+1));
-                messages[i]=(userColourChoice[i] + " is CORRECT in place n." + (i+1));
+                messages[i] = (userColourChoice[i] + " is CORRECT in place n." + (i+1));
                 
-        
         } else  if (computerColourChoice.indexOf(userColourChoice[i])!==(-1) && userColourChoice[i]!==computerColourChoice[i]) {
-    
-                console.log(userColourChoice[i] + " is PRESENT but not in position: " + (i+1));
-                messages[i]=(userColourChoice[i] + " is PRESENT but not in position: " + (i+1));
+
+                messages[i] = (userColourChoice[i] + " is PRESENT but not in position: " + (i+1));
     
         } else {
-                console.log(userColourChoice[i] + " is NOT part of solution");
-                messages[i]=(userColourChoice[i] + " is NOT part of solution");
+
+                messages[i] = (userColourChoice[i] + " is NOT part of solution");
+
         }
+
         messagesCollector.push(messages[i]);
     }
     
-
+    //
     alert(messages.join("\n"));
-    messagesCollector.push("\n***************Round"+ roundCounter +"*****************\n");
+    messagesCollector.push("\n***************Round" + roundCounter + "*****************\n");
     alert(messagesCollector.toString());
 
+    //Counters inside the main do-while loop
     runningGameLevelChoiche--;
     roundCounter++;
 
+    // Win loss control part
     if (userColourChoice[0]===computerColourChoice[0] && 
         userColourChoice[1]===computerColourChoice[1] && 
         userColourChoice[2]===computerColourChoice[2] && 
@@ -122,7 +131,7 @@ do {
 
     } else  if (runningGameLevelChoiche===0) {
 
-        userHasWon=false;
+        userHasWon = false;
         alert ("Sorry you lost!");
         break;
     }
