35c51e9af2441fb4121ca125e156754b4a7a6c47
Davide Baldi <d.baldi88@gmail.com>
2023-11-30 10:51:10 +0100

    clean up messages

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 const COLOUR_DATABASE = ["RED","ORANGE","YELLOW", "GREEN", "BLUE", "PURPLE","PINK"];
 const LEVELS_DATABASE = ["easy","medium","hard","mastermind"];
 let copy_COLOUR_DATABASE = Array.from(COLOUR_DATABASE);
-let computerColourChoice = new Array (4); ;
+let computerColourChoice = new Array (4);
 let userColourChoice = new Array(4);
 let messages = new Array (4);
 let messagesCollector = [];
@@ -22,16 +22,19 @@ alert("***** Welcome at Mastermind *****");
 alert("***Instructions: guess the 4 colours the computer has choosen. ***" +
 "\n ***Choose within: " + COLOUR_DATABASE.join(" ") +
 "***\n***Hard level adds: " + EXTRA_COLOUR_ONE +
-"Level mastermind adds: " + EXTRA_COLOUR_ONE + " and " + EXTRA_COLOUR_TWO +
+" Level mastermind adds: " + EXTRA_COLOUR_ONE + " and " + EXTRA_COLOUR_TWO +
  "*** \n***The colours choosen by the computer will be ALL different***");
 
 //Difficulty-level choiches with validation
 do {
+
     difficultyLevelChoiche=prompt("Choose a level within: \n easy (max 14 attempts) \n medium (max 10 attempts) \n hard (max 7 attempts + 1 extra colour) \n mastermind (max 4 attempts + 2 extra colours)");
 
     if(!difficultyLevelChoiche) {
+
             displayAlertWrongInput("To quit the game exit or refresh page!");
         } else if (LEVELS_DATABASE.includes(difficultyLevelChoiche)===false) {
+
             displayAlertWrongInput("Valid inputs are:\n"+ LEVELS_DATABASE.join(" ") + "\nInputs are case sensitive");
         }
 
@@ -39,17 +42,21 @@ do {
 
 //Difficulty-level menu
 switch (difficultyLevelChoiche) {
+
     case "easy":
         runningGameLevelChoiche = 14;
         break;
+
     case "medium":
         runningGameLevelChoiche = 10;
         break;
+
     case "hard":
         runningGameLevelChoiche = 7;
         copy_COLOUR_DATABASE.push(EXTRA_COLOUR_ONE);
         COLOUR_DATABASE.push(EXTRA_COLOUR_ONE);
         break;
+
     case "mastermind":
         runningGameLevelChoiche= 4;
         copy_COLOUR_DATABASE.push(EXTRA_COLOUR_ONE);
@@ -57,6 +64,7 @@ switch (difficultyLevelChoiche) {
         COLOUR_DATABASE.push(EXTRA_COLOUR_ONE);
         COLOUR_DATABASE.push(EXTRA_COLOUR_TWO);
         break;
+
     default:
         break;
 }
@@ -72,7 +80,7 @@ do {
 
     for ( let i=0; i<userColourChoice.length; i++ ) {
         
-        //User´choiches with validation
+        //User´s choiches with validation
         do {
            
             userColourChoice[i] = prompt("Choose within: \n" + COLOUR_DATABASE.join(" ") + 
@@ -82,7 +90,7 @@ do {
             "----------");
 
             if (!userColourChoice[i]) {
-                
+
                     displayAlertWrongInput("To quit the game exit or refresh page!");
 
                 } else if (COLOUR_DATABASE.includes(userColourChoice[i])===false) {
@@ -96,25 +104,25 @@ do {
         //User´s input-computer colours match
         if (userColourChoice[i]===computerColourChoice[i]) {
 
-                messages[i] = (userColourChoice[i] + " is CORRECT in place n." + (i+1));
+                messages[i] = (userColourChoice[i] + " is CORRECT in place n." + (i+1) + ". ");
                 
         } else  if (computerColourChoice.indexOf(userColourChoice[i])!==(-1) && userColourChoice[i]!==computerColourChoice[i]) {
 
-                messages[i] = (userColourChoice[i] + " is PRESENT but not in position: " + (i+1));
+                messages[i] = (userColourChoice[i] + " is PRESENT but not in position: " + (i+1)+ ". ");
     
         } else {
 
-                messages[i] = (userColourChoice[i] + " is NOT part of solution");
+                messages[i] = (userColourChoice[i] + " is NOT part of solution. ");
 
         }
 
         messagesCollector.push(messages[i]);
     }
     
-    //
+    //Match messages collection and display
     alert(messages.join("\n"));
     messagesCollector.push("\n***************Round" + roundCounter + "*****************\n");
-    alert(messagesCollector.toString());
+    alert(messagesCollector.join(""));
 
     //Counters inside the main do-while loop
     runningGameLevelChoiche--;
